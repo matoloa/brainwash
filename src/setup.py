bf3082bfb67d1a6a5d8831a723ae57e956c8f19e
Jonathan Anderson <emailjonathananderson-github@yahoo.com>
2023-11-21 09:54:16 +0100

    cx_freeze: setup.py

---
+++ src/setup.py
@@ -0,0 +1,44 @@
+import sys
+from setuptools import find_packages
+from cx_Freeze import setup, Executable
+
+# base="Win32GUI" should be used only for Windows GUI app
+print(f"sys.platform: {sys.platform}")
+base = "Win32GUI" if sys.platform == "win32" else None
+
+
+# Include the path to the Python script you want to freeze.
+script_path = "main.py"
+# Additional files/directories that should be included in the distribution.
+# You may need to add other dependencies or data files here.
+include_files = ["lib/"]
+# Create an executable.
+exe = Executable(
+    script=script_path,
+    #base=base,
+    #targetName="hello.exe"  # The name of the output executable.
+)
+# Setup cx_Freeze options.
+options = {
+    "build_exe": {
+        "includes": [],
+        "excludes": [],
+        "packages": ["pyabf", "neo", "tqdm", "sklearn"],
+        "include_files": include_files
+    }
+}
+
+
+# Call the setup function.
+setup(
+    name="Brainwash",
+    version="0.1",
+    description="",
+    #packages=find_packages(where="src"),
+    #package_dir={"": "src"},
+    #include_package_data=True,
+    options=options,
+    executables=[exe],
+    base=base
+)
+
