0b0e9884da0c1e88514e86f8da2c736fad28fc87
Matol <mats.olof.andersson@gmail.com>
2025-04-21 14:00:34 +0200

    WIP

--- src/setup.py
+++ src/setup.py
@@ -16,12 +16,13 @@ base = "Win32GUI" if sys.platform == "win32" else None
 # Include the path to the Python script you want to freeze.
 script_path = "main.py"
 # Find the vcomp140.dll file in the system
-vcomp140_dll_path = os.path.join(os.environ['windir'], 'System32', 'vcomp140.dll')
-print(f"vcomp140_dll_path: {vcomp140_dll_path}, exists: {os.path.exists(vcomp140_dll_path)}")
+#vcomp140_dll_path = os.path.join(os.environ['windir'], 'System32', 'vcomp140.dll')
+#print(f"vcomp140_dll_path: {vcomp140_dll_path}, exists: {os.path.exists(vcomp140_dll_path)}")
 
 # Additional files/directories that should be included in the distribution.
 # You may need to add other dependencies or data files here.
-include_files = [f"{vcomp140_dll_path}", "lib/", ("../pyproject.toml", "lib/pyproject.toml")]
+include_files = []
+#include_files = [f"{vcomp140_dll_path}", "lib/", ("../pyproject.toml", "lib/pyproject.toml")]
 		 #("/home/jonathan/mambaforge/envs/brainwash/lib/libcblas.so", "lib/libcblas.so"), ("/home/jonathan/mambaforge/envs/brainwash/lib/libcblas.so.3", "lib/libcblas.so.3")]
 # Create an executable.
 exe = Executable(
