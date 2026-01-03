b98051ae1aecfa6a8cffd917b8dac9e3c4465f04
Jonathan Anderson <emailjonathananderson-github@yahoo.com>
2026-01-03 15:37:29 +0100

    Update build_with_cxfreeze_multiarch_setup_old.py

--- src/build_with_cxfreeze_multiarch_setup_old.py
+++ src/build_with_cxfreeze_multiarch_setup_old.py
@@ -1,19 +1,19 @@
-'''
+"""
 Usage:
     with cx-freeze==8.5.0
     python build_with_cxfreeze_multiarch_setup.py bdist_appimage > cxbuild_appimage.log
     python build_with_cxfreeze_multiarch_setup.py bdist_msi > cxbuild_msi.log
-'''
+"""
 
-import sys
 import os
-from setuptools import find_packages
-from cx_Freeze import setup, Executable
-import toml  # for reading pyproject.toml
+import sys
 
+import toml  # for reading pyproject.toml
+from cx_Freeze import Executable, setup
+from setuptools import find_packages
 
 pyproject = toml.load("../pyproject.toml")
-version = pyproject['project']['version']
+version = pyproject["project"]["version"]
 
 # base="Win32GUI" should be used only for Windows GUI app
 print(f"sys.platform: {sys.platform}")
@@ -28,18 +28,18 @@ include_files = [("../pyproject.toml", "lib/pyproject.toml"), "lib/"]
 
 # windows build
 # Find the vcomp140.dll file in the system
-#vcomp140_dll_path = os.path.join(os.environ['windir'], 'System32', 'vcomp140.dll')
-#print(f"vcomp140_dll_path: {vcomp140_dll_path}, exists: {os.path.exists(vcomp140_dll_path)}")
+# vcomp140_dll_path = os.path.join(os.environ['windir'], 'System32', 'vcomp140.dll')
+# print(f"vcomp140_dll_path: {vcomp140_dll_path}, exists: {os.path.exists(vcomp140_dll_path)}")
 
-# Linux: commented for now. No signs of malfunction. If needed, change to proper paths. We dropped conda in Linux for pip and venv. 
-		 #("/home/jonathan/mambaforge/envs/brainwash/lib/libcblas.so", "lib/libcblas.so"), ("/home/jonathan/mambaforge/envs/brainwash/lib/libcblas.so.3", "lib/libcblas.so.3")]
+# Linux: commented for now. No signs of malfunction. If needed, change to proper paths. We dropped conda in Linux for pip and venv.
+# ("/home/jonathan/mambaforge/envs/brainwash/lib/libcblas.so", "lib/libcblas.so"), ("/home/jonathan/mambaforge/envs/brainwash/lib/libcblas.so.3", "lib/libcblas.so.3")]
 
 
 # Create an executable.
 exe = Executable(
     script=script_path,
     base=base,
-    #targetName="hello.exe"  # The name of the output executable.
+    # targetName="hello.exe"  # The name of the output executable.
 )
 
 # Setup cx_Freeze options.
@@ -48,7 +48,7 @@ options = {
         "includes": [],
         "excludes": ["tkinter", "email", "pytest"],
         "packages": ["pyabf", "igor2", "tqdm", "sklearn", "numpy", "scipy", "seaborn"],
-        "include_files": include_files
+        "include_files": include_files,
     }
 }
 
@@ -58,10 +58,9 @@ setup(
     name="brainwash",
     version=version,
     description="",
-    #packages=find_packages(where="src"),
-    #package_dir={"": "src"},
-    #include_package_data=True,
+    # packages=find_packages(where="src"),
+    # package_dir={"": "src"},
+    # include_package_data=True,
     options=options,
     executables=[exe],
 )
-
